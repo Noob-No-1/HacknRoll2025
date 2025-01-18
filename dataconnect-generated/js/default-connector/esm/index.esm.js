79760c3736248402aff63495776d61eef9cd6e8d
Stephanie <jianglh0425@outlook.com>
2025-01-18 13:19:30 +0800

    firebase initialization done

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'mma-hacknroll',
+  location: 'us-central1'
+};
+
